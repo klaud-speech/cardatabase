2617991f1dc17b7131120e607cd498b69225d085
sahngwoon_lee <sahngwoon.lee@llsollu.com>
2023-08-23 14:45:15 +0900

    log-back

--- src/main/java/com/speech/cardatabase/web/ProjectController.java
+++ src/main/java/com/speech/cardatabase/web/ProjectController.java
@@ -3,6 +3,8 @@ package com.speech.cardatabase.web;
 import com.speech.cardatabase.domain.Owner;
 import com.speech.cardatabase.domain.Project;
 import com.speech.cardatabase.domain.ProjectRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,8 @@ public class ProjectController {
 
     @Autowired
     private ProjectRepository projectRepository;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProjectController.class);
 
     @RequestMapping(value = "/project", method = RequestMethod.POST)
     public ResponseEntity<?> setProjectInfo (@RequestBody Map<String, Object> postData){
@@ -56,6 +60,8 @@ public class ProjectController {
     public ResponseEntity<?> setup (@RequestBody Map<String, Object> postData) {
 
         final String[] projectInfo = new String[postData.size()];
+
+        LOGGER.info("url 메서드가 호출되었습니다.");
 
         /*
         postData.entrySet().forEach(map -> {
